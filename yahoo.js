255847e90e371f4362c82a4ddc3e15f2f0b45baa
Ionică Bizău <bizauionica@yahoo.com>
2013-10-20 14:56:25 +0300

    Fixes.

--- yahoo.js
+++ yahoo.js
@@ -49,6 +49,10 @@ var onError = function(msg) {
     if (module.exports.error) {
         module.exports.error(msg);
     }
+
+    if (callback) {
+        callback(msg);
+    }
 };
 
 
@@ -72,59 +76,8 @@ yahoo.weather = function(woeid, unit, callback) {
         var parser = new xml2js.Parser();
 
         parser.parseString(body, function (err, result) {
-
-
-            try {
-
-                var weather = {
-
-                    //The date
-                    date        : result.rss.channel[0].item[0].pubDate[0],
-
-                    //Some general informations
-                    //@see http://developer.yahoo.com/weather/#channel
-                    location    : result.rss.channel[0]['yweather:location'][0]['$'],
-                    units       : result.rss.channel[0]['yweather:units'][0]['$'],
-                    wind        : result.rss.channel[0]['yweather:wind'][0]['$'],
-                    atmosphere  : result.rss.channel[0]['yweather:atmosphere'][0]['$'],
-                    astronomy   : result.rss.channel[0]['yweather:astronomy'][0]['$'],
-
-                    //Item elements
-                    //@see http://developer.yahoo.com/weather/#item
-                    condition   : result.rss.channel[0].item[0]['yweather:condition'][0]['$'], //Current conditions
-                    description : result.rss.channel[0].item[0].description[0], //HTML widget
-
-                    days        : []
-                };
-
-                //We add latitude and longitude info to the location object
-                weather.location.lat  = result.rss.channel[0].item[0]['geo:lat'][0];
-                weather.location.long = result.rss.channel[0].item[0]['geo:long'][0];
-
-
-                //Extract forecasts for nexts days
-                var forecasts = result.rss.channel[0].item[0]['yweather:forecast'];
-                for(var i=0; i<forecasts.length; i++) {
-
-                    var forecast = forecasts[i]['$'];
-                    weather.days.push({
-                        day  : forecast.day, //Day of the week (english, 3 letters, ex: Wed)
-                        date : forecast.date, //Date, (english, dd Mmm yyyy)
-                        low  : forecast.low,
-                        high : forecast.high,
-                        text : forecast.text, //Description of conditions (ex: Partly Cloudy)
-                        code : forecast.code, //@see http://developer.yahoo.com/weather/#codes
-                    });
-
-                }
-
-                callback(weather);
-
-            } catch(e) {
-                onError('Failed to find weather');
-                return;
-            }
-
+            if (err) { return onError('Failed to find weather: ' + err.messsage || err, callback); }
+            callback(null, result);
         });
     });
 };
@@ -156,14 +109,14 @@ yahoo.where = function(options, callback) {
         if (body) {
             parser.parseString(body, function (err, result) {
                 try {
-                    var woeid = result.places.place[0].woeid[0];
+                    var woeid = result.place.locality1["@"].woeid;
                     callback(woeid);
                 } catch(e) {
-                    onError('Failed to fetch woeid');
+                    onError('Failed to fetch woeid', callback);
                 }
             });
         } else {
-            onError(error);
+            onError(error, callback);
         }
     });
 };
